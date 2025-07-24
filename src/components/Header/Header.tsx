20186bf94e338aa6a961e32eeff4c183172676b5
OleksandrHridzhak <sgridzak@gmail.com>
2025-07-24 09:26:59 +0300

    header is fully adaptive now

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,14 +5,31 @@ import SearchBtn from './SearchBtn'
 
 const Header: React.FC = ({}) => {
   return (
-    <header className='flex bg-light-c w-full justify-between items-center px-4 h-15 border-b border-gray-c'>
-        <Link href="/" className="font-bold text-xl">
+    <header className='flex bg-light-c w-full  items-center h-17 border-b xl:border-b-[1.5px] border-b-gray-c'>
+      <div className='flex justify-between w-full px-5 md:px-7 mx-auto max-w-screen-xl  items-center'>
+        <Link href="/" className="font-bold text-2xl md:text-3xl">
             cerk
         </Link>
-        <div className='flex items-center gap-1'>
+        <div className='flex items-center gap-1 xl:gap-7'>
+          <nav>
+                <ul className='hidden xl:flex gap-7'>
+                    <li>
+                        <Link href="/about" className="text-gray-c font-semibold hover:text-gray-800 transition-colors duration-200">
+                            About
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/contact" className="text-gray-c font-semibold hover:text-gray-800 transition-colors duration-200">
+                            Contact
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+
             <SearchBtn />
             <BurgerMenu/>
         </div>
+      </div>
     </header>
   )
 }
