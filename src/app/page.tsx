dd05578125436e807bbe2d60fc350ec00505a259
OleksandrHridzhak <sgridzak@gmail.com>
2026-01-08 10:51:11 +0200

    feat: changed main page qoute

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   return (
     <div className="">
       <h1 className="text-center text-gray-c text-2xl sm:text-3xl md:text-4xl md:py-3 font-bold  mx-auto my-10 px-2" >
-        “I don&apos;t like it because<br/> it&apos;s boring”
+        “From passive consumption to active production.”
       </h1>
       <ArticleList articles={articles} />
     </div>
